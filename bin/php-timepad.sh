8503729053515e9e5abe0a15c4213684373ab88e
Alexander Rybakov <a.rybakov94@gmail.com>
2015-07-30 19:50:08 +0300

    В APIClientException json ответа переводится в объект.

--- bin/php-timepad.sh
+++ bin/php-timepad.sh
@@ -26,6 +26,6 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="$@ generate -t modules/swagger-codegen/src/main/resources/php -i http://large-friction.surge.sh/swagger.json -l php -o samples/client/timepad/php"
+ags="$@ generate -t modules/swagger-codegen/src/main/resources/php -i https://api.timepad.ru/doc/swagger.json -l php -o samples/client/timepad/php"
 
 java $JAVA_OPTS -jar $executable $ags
